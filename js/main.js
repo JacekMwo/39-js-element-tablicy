968b99fefcecc7102422a3d4296cf500637b213e
JacekMwo <jckbanach@gmail.com>
2023-05-09 16:34:48 +0200

    39-js-element-tablicy

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Zdeklaruj zmienną o dowolnej nazwie i przypisz do niej tablicę składającą się z 5 liczb.
+const numery = [2, 4, 6, 8, 10]
+
+// Napisz funkcję, która przyjmuje tablicę jako parametr. Funkcja ma zwracać pierwszy element tablicy.
+function pierwszaLiczba(array) {
+    return array[0];
+}
+
+// Wynik działania funkcji wyświetl w konsoli.
+console.log(pierwszaLiczba(numery));    
